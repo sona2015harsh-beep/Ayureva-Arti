31e9d670556cec1b35670481d183bf747d30f66d
Ashwani Kumar <join2campus@gmail.com>
2026-07-05 11:08:19 +0530

    chore: remove Download App link from navigation bar

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -81,13 +81,6 @@ export default function Header() {
               >
                 Contact
               </Link>
-              <Link
-                href="/mobile-app"
-                className="text-gray-700 hover:text-green-600 font-medium transition-colors"
-                onClick={(e) => handleNavClick("/mobile-app", e)}
-              >
-                Download App
-              </Link>
             </nav>
 
             <Link href="/contact" onClick={handleConsultationClick}>
